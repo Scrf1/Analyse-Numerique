3f2562d98447fc218b67aa2ac84de579aafd55c5
Sourec Seraphin <srgseraphin14@gmail.com>
2019-05-13 15:29:28 +0100

    Interface Fonction2

---
+++ src/interfaces/Fonction2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author SCrf1
+ */
+@FunctionalInterface
+public interface Fonction2 
+{
+    double calcul(double x, double y);
+}
